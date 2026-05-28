4ff1a2c79c75b301454b5cdb92ee1c2369d8790a
Sabarish2317 <Sabarish_7@protonmail.com>
2026-05-28 08:41:12 +0530

    fix

--- src/pages/productsPage/ProductsPage.tsx
+++ src/pages/productsPage/ProductsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState, useCallback } from 'react'
+import { useMemo, useState, useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { motion, AnimatePresence } from 'framer-motion'
